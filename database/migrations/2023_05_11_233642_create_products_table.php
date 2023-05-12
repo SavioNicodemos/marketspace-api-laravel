1fe9c61060e0e883888b1d245167149e3991d160
Savio Nicodemos <nicodemosgcosta@gmail.com>
2023-05-12 01:14:39 +0100

    Create Products table

---
+++ database/migrations/2023_05_11_233642_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->longText('description');
+            $table->boolean('is_new');
+            $table->integer('price');
+            $table->boolean('accept_trade');
+            $table->foreignUuid('user_id');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
