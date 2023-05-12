6508fd7204c859b1deb029c6f0d369480f78f9cb
Savio Nicodemos <nicodemosgcosta@gmail.com>
2023-05-12 02:00:32 +0100

    Create many/many relations to product and payments

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentMethod extends Model
 {
+
+    /**
+     * The products that belong to the payment method.
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
