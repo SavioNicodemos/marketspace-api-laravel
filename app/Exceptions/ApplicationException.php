f64ef4048cd4c75f62dd70419a34eeae8f612adb
Savio Nicodemos <nicodemosgcosta@gmail.com>
2023-05-19 01:46:45 +0100

    Add General Application exception

---
+++ app/Exceptions/ApplicationException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class ApplicationException extends Exception
+{
+    protected $message;
+    protected $code;
+
+    public function __construct($message, $code = 400)
+    {
+        $this->message = $message;
+        $this->code = $code;
+    }
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return JsonResponse
+     */
+    public function render(): JsonResponse
+    {
+        return response()->json(['message' => ucfirst($this->message)], $this->code);
+    }
+}
